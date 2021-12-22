718ea4e1c49c3e4ad0f0f5d21992942fb9ae1e11
dekeyofspace <83281012+dekeyofspace@users.noreply.github.com>
2021-12-22 12:00:13 +0300

    123

---
+++ signin.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Luminarskaya2
+{
+    partial class signin
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.p_top = new System.Windows.Forms.Panel();
+            this.tb_login = new System.Windows.Forms.TextBox();
+            this.tb_password = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cb_password = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // p_top
+            // 
+            this.p_top.BackColor = System.Drawing.Color.Gold;
+            this.p_top.Dock = System.Windows.Forms.DockStyle.Top;
+            this.p_top.Location = new System.Drawing.Point(0, 0);
+            this.p_top.Name = "p_top";
+            this.p_top.Size = new System.Drawing.Size(784, 100);
+            this.p_top.TabIndex = 0;
+            // 
+            // tb_login
+            // 
+            this.tb_login.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tb_login.Location = new System.Drawing.Point(373, 195);
+            this.tb_login.Name = "tb_login";
+            this.tb_login.Size = new System.Drawing.Size(182, 37);
+            this.tb_login.TabIndex = 1;
+            this.tb_login.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // tb_password
+            // 
+            this.tb_password.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tb_password.Location = new System.Drawing.Point(373, 256);
+            this.tb_password.Name = "tb_password";
+            this.tb_password.Size = new System.Drawing.Size(182, 37);
+            this.tb_password.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(272, 198);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 29);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Логин:";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(403, 322);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 40);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Войти";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(272, 259);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 29);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Пароль:";
+            // 
+            // cb_password
+            // 
+            this.cb_password.AutoSize = true;
+            this.cb_password.Location = new System.Drawing.Point(373, 299);
+            this.cb_password.Name = "cb_password";
+            this.cb_password.Size = new System.Drawing.Size(114, 17);
+            this.cb_password.TabIndex = 6;
+            this.cb_password.Text = "Показать пароль";
+            this.cb_password.UseVisualStyleBackColor = true;
+            this.cb_password.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // signin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.cb_password);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_password);
+            this.Controls.Add(this.tb_login);
+            this.Controls.Add(this.p_top);
+            this.MaximumSize = new System.Drawing.Size(800, 600);
+            this.MinimumSize = new System.Drawing.Size(800, 600);
+            this.Name = "signin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Форма авторизации";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel p_top;
+        private System.Windows.Forms.TextBox tb_login;
+        private System.Windows.Forms.TextBox tb_password;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox cb_password;
+    }
+}
+
